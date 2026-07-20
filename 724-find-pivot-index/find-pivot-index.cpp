163190eb6918f9c59db9937cc8113f4f54708bcc
Giri Sai Surya Trishal <trishalgiri2006@gmail.com>
2026-07-20 18:15:51 +0530

    Time: 4 ms (14.40%) | Memory: 36.4 MB (17.43%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+
+    int pivotIndex(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> pre(n);
+        pre[0]=nums[0];
+        for(int i=1;i<n;i++){
+            pre[i]=pre[i-1]+nums[i];
+        }
+        for(int j=0;j<n;j++){
+            int leftsum=0;
+            int rightsum=0;
+            if(j==0){
+               rightsum=pre[n-1]-pre[0];
+            }
+            else if(j==n-1){
+                leftsum=pre[j-1];
+            }
+            else{
+                leftsum=pre[j]-nums[j];
+                rightsum=pre[n-1]-pre[j];
+            }
+            if(leftsum==rightsum){
+                return j;
+            }
+        }
+        return -1;
+
+    }
+};
